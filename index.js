938d8d723b343ae94153234b1431ff339dd7c58a
Eduardo <el_alienx@hotmail.com>
2024-03-17 12:35:36 +0100

    finish the boilerplate before adding Docker (12:58)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Node modules
 import express, { response } from "express";
 
+// Project files
+import client from "./database.js";
+
 // Properties
 const port = 1337;
 const app = express();
@@ -9,14 +12,46 @@ const app = express();
 app.use(express.json());
 
 // Routes
-app.get("/", (request, response) => response.sendStatus(200));
+app.get("/", async (request, response) => {
+  const query = "SELECT * FROM assigments2";
+  let data = "";
 
-app.post("/", (request, response) => {
+  try {
+    data = await client.query(query);
+    response.sendStatus(200).send(data.rows);
+  } catch (error) {
+    console.error(error);
+    response.sendStatus(500);
+  }
+});
+
+app.get("/setup", async (request, response) => {
+  const query =
+    "CREATE TABLE assigments2(id SERIAL PRIMARY KEY, assigment_name VARCHAR(100), company_name VARCHAR(100))";
+  const message = "Postgres initialized table assigments2";
+
+  try {
+    await client.query(query);
+    response.sendStatus(200).send({ message: message });
+  } catch (error) {
+    console.error(error);
+    response.sendStatus(500);
+  }
+});
+
+app.post("/", async (request, response) => {
   const { assigment_name, company_name } = request.body;
+  const query =
+    "INSERT INTO assigments2 (assigment_name, company_name) VALUES ($1, $2)";
+  const message = "Postgres added new assigment";
 
-  response.status(200).send({
-    message: `New assigment ${assigment_name} for ${company_name}`,
-  });
+  try {
+    await client.query(query, [assigment_name, company_name]);
+    response.sendStatus(200).send({ message: message });
+  } catch (error) {
+    console.error(error);
+    response.sendStatus(500);
+  }
 });
 
 // Start server
