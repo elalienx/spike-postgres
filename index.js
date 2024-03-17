74866fd694f99b9ea17f999efc73ddae8f0211f1
Eduardo <el_alienx@hotmail.com>
2024-03-17 11:59:30 +0100

    sent first post requesrt (7:30 of tutorial)

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // Node modules
-import express from "express";
+import express, { response } from "express";
 
 // Properties
 const port = 1337;
 const app = express();
 
+// Setup
+app.use(express.json());
+
 // Routes
 app.get("/", (request, response) => response.sendStatus(200));
+
+app.post("/", (request, response) => {
+  const { assigment_name, company_name } = request.body;
+
+  response.status(200).send({
+    message: `New assigment ${assigment_name} for ${company_name}`,
+  });
+});
 
 // Start server
 app.listen(port, () => console.log(`Server started on ${port}`));
