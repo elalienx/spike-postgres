d61e2497a8e2eda214c3bd152d704af48ac9a38b
Eduardo Alvarez <el_alienx@hotmail.com>
2024-03-23 15:42:24 +0100

    finally fix the assignments table

--- src/endpoints/getSetupTable.js
+++ src/endpoints/getSetupTable.js
@@ -10,7 +10,7 @@ import pool from "../database.js";
 export default async function getSetupTable(response) {
   const query = `CREATE TABLE IF NOT EXISTS assignments (
     id SERIAL PRIMARY KEY, 
-    date_created TIMESTAMP DEFAULT CURRENT_DATE,
+    date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     assignment_name VARCHAR(50),
     company_name VARCHAR(50),
     company_image_url VARCHAR(255)
