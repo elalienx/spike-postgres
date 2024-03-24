618307a740e6d0dee3c756e4d5e9826936181f54
Eduardo Alvarez <el_alienx@hotmail.com>
2024-03-24 15:04:21 +0100

    added code for error handling and forced the db to stop the server if the db cannot be intialized.

---
+++ src/credentials.js
@@ -0,0 +1,9 @@
+const credentials = {
+  host: "database",
+  port: 5432,
+  database: "database123",
+  user: "user123",
+  password: "password123",
+};
+
+export default credentials;
--- src/database.js
+++ src/database.js
@@ -1,23 +1,30 @@
 // Node modules
 import pkg from "pg";
 
+// Project files
+import credentials from "./credentials.js";
+
 // Properties
 const { Pool } = pkg;
-const credentials = {
-  host: "database",
-  port: 5432,
-  database: "database123",
-  user: "user123",
-  password: "password123",
-};
 const pool = new Pool(credentials);
+const messages = {
+  success: `Posgress server started on port ${credentials.port}`,
+  hostError: `Error: Ensure your environment has a Postgres server. If using Docker, verify that the host called ${credentials.host} matches the Docker container name of the database`,
+  portError: `Error: Check if the port ${credentials.port} matches the exposed port in Docker`,
+  databaseError: `Error: The database called ${credentials.database} does not exist on the current Postgress server`,
+  authError: `Error: The user ${credentials.user} or its password do not match the records in the database called ${credentials.database}`,
+};
 
 try {
-  pool.connect();
-  console.info(`Posgress server started on port ${credentials.port}`);
+  await pool.connect();
+  console.info(messages.success);
 } catch (error) {
-  if (error.err === -3008) console.error("Check if your enviroment has Postgres server");
-  console.error(error);
+  if (error.code === "ENOTFOUND") console.error(messages.hostError);
+  if (error.code === "ECONNREFUSED") console.error(messages.portError);
+  if (error.code === "3D000") console.error(messages.databaseError);
+  if (error.code === "28P01") console.error(messages.databaseError);
+
+  throw new Error(error);
 }
 
 export default pool;
